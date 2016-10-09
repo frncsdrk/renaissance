54cbe28065b0a9fe1b8829b8b1f4626b1d6d0deb
frncsdrk <carlsmoh@gmail.com>
2016-10-09 15:53:43 +0200

    Add Attachables to allow multiple instances of components

--- demo/scripts/test-component-plain.js
+++ demo/scripts/test-component-plain.js
@@ -14,21 +14,31 @@ component.after('initialize', function() {
 
 var testComponent = potion.defineComponent(
     function testComponent() {
+        var self = this;
         // this.$: element reference
         // keyword: after
-        this.setMeta({});
+        this.setMeta({
+            // content: self.node.innerHTML
+        });
 
         this.add = function() {};
 
+        this.rename = function() {
+            self.node.innerHTML = 'yolo button';
+        };
+
         // keyword: after
-        // var self = this;
         this.after('init', function(component) {
             // keyword: on
-            component.on('click', function() { console.log('testComponent click'); });
+            component.on('click', function() {
+                console.log('testComponent click');
+                // console.log('testComponent meta content:', self.meta.content);
+                self.rename();
+            });
             console.log('component:', component);
             // component.node.addEventListener('click', function() { console.log('testComponent click'); });
         });
 
         // return this;
     }
-)
+);
