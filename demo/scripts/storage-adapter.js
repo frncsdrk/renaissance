82e5ea8cafdb1d925afd7e822a1ab47d162fd406
frncsdrk <carlsmoh@gmail.com>
2016-10-09 22:21:33 +0200

    Add storage adapter support
    TODO: need way to utilize templates -> add template adapter support

--- demo/scripts/storage-adapter.js
+++ demo/scripts/storage-adapter.js
@@ -1,9 +1,26 @@
 // storage adapter
 function localStorageAdapter() {
-    this.save = function() {};
-    this.update = function() {};
-    this.get = function() {};
-    this.remove = function() {};
+    this.save = function(key, value) {
+        localStorage.setItem(key, value);
+        return true;
+    };
+    this.update = function(key, value) {
+        if (!localStorage.getItem(key)) {
+            return false;
+        }
+        localStorage.setItem(key, value);
+        return true;
+    };
+    this.get = function(key) {
+        return localStorage.getItem(key);
+    };
+    this.remove = function(key) {
+        localStorage.removeItem(key);
+        return true;
+    };
+    this.clear = function() {
+        localStorage.clear();
+    };
 }
 
-potion.registerAdapter(localStorageAdapter);
+potion.registerAdapter(potion.storage, 'localStorage', localStorageAdapter);
