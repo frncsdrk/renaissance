f50552a1a1f64a071640775cf094314696b9cdcc
frncsdrk <carlsmoh@gmail.com>
2016-10-23 19:35:39 +0200

    Update appendHTML drink
    for allowing callbacks especially for adding components to dynamically created nodes

--- demo/scripts/rjs/append_html_drink.js
+++ demo/scripts/rjs/append_html_drink.js
@@ -3,13 +3,15 @@
 
 define(
     [
-        'lib/drink'
+        'potion/drink'
     ]
     , function(registerDrink) {
-        function appendHTML(html, node) {
+        function appendHTML(html, node, cb) {
             var elem = document.createElement('div');
             elem.innerHTML = html;
-            var children = elem.childNodes;
+            var children = elem.children;
+
+            cb(children);
 
             for (var i = 0; i < children.length; i++) {
                 node.appendChild(children[i]);
