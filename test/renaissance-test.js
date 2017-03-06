eda36734b8451b3266b02307137cb8b1ab6f607f
frncsdrk <carlsmoh@gmail.com>
2017-03-06 23:02:16 +0100

    Add suite
    finish basic testing
    
    TODO: reporter

--- test/renaissance-test.js
+++ test/renaissance-test.js
@@ -5,34 +5,83 @@ define(
     ]
     , function(renaissance) {
         /**
-         * test
-         * @param {object} env
+         * suite
+         * @param {string} name
          * @param {function} cb
-         * @param {string} verb
-         * @param {*} expected result
-         * @returns {boolean}
+         * @returns {object} results
          */
-        function test(env, cb, verb, expectedResult) {
-            var res = cb(env);
+        function suite(name, cb) {
+            var self = this;
+            self.result = [];
 
-            function shouldEqual(res, should) {
-                if (res == should) {
-                    return true;
+            /**
+             * test
+             * @param {string} name
+             * @param {function} cb
+             * @param {string} verb
+             * @param {*} expected result
+             * @returns {boolean}
+             */
+            function test(name, cb, verb, expectedResult) {
+                var testResult = {
+                    name: name
+                    , cb: cb
+                    , verb: verb
+                    , expectedResult: expectedResult
+                };
+
+                var res = cb();
+
+                function shouldEqual(res, should) {
+                    if (res == should) {
+                        return true;
+                    }
+                    return false;
                 }
-                return false;
+
+                function shouldNot() {
+                    if (res != should) {
+                        return true;
+                    }
+                    return false;
+                }
+
+                function shouldTypeOf(res, should) {
+                    if (typeof res === should) {
+                        return true;
+                    }
+                    return false;
+                }
+
+                switch (verb) {
+                    case 'equal':
+                        // return shouldEqual(res, expectedResult);
+                        testResult.result = shouldEqual(res, expectedResult);
+                        break;
+                    case 'not':
+                        testResult.result = shouldNot(res, expectedResult);
+                        break;
+                    case 'typeof':
+                        // return shouldTypeOf(res, expectedResult);
+                        testResult.result = shouldTypeOf(res, expectedResult);
+                        break;
+                    default:
+                        testResult.result = false;
+                        break;
+                }
+
+                self.result.push(testResult);
+                // return testResult;
+                return true;
             }
 
-            switch (verb) {
-                case 'equal':
-                    return shouldEqual(res, expectedResult);
-                    break;
-                default:
-                    break;
-            }
+            cb(test);
+
+            return this.result;
         }
 
         return function() {
-            renaissance.registerPlugin('test', test);
+            renaissance.registerPlugin('suite', suite);
         };
     }
 );
