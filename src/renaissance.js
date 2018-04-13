e513d4b3e2c4cf275c9bcc73de631c9ee4a603eb
frncsdrk <carlsmoh@gmail.com>
2018-04-13 18:58:53 +0200

    Fix component class
    add example code to demo

--- src/renaissance.js
+++ src/renaissance.js
@@ -5,6 +5,80 @@ import 'babel-core/register'
 
 let renaissance = {};
 
+class Component {
+    /**
+     * component
+     * @constructor
+     * @returns {object} this
+     */
+    constructor() {
+        this.beforeMap = {};
+        this.afterMap = {};
+        this.node = null;
+
+        this.init && this.after('init', this.init);
+
+        return this;
+    }
+    /**
+     * set node
+     * @param {object} node
+     * @returns {*}
+     */
+    setNode(node) {
+        this.node = node;
+    }
+    /**
+     * add an event handler on component
+     * @param {string} nodeEvent
+     * @param {function} callback
+     * @returns {*}
+     */
+    on(nodeEvent, callback) {
+        this.node.addEventListener(nodeEvent, callback);
+    }
+    /**
+     * expose an event to other components and make it triggerable
+     * (for custom events)
+     * @param {string} eventName
+     * @param {function} callback
+     * @returns {*}
+     */
+    expose(eventName, callback) {
+        document.addEventListener(evName, function(e) { cb(e, e.detail); });
+    }
+    // trigger exposed events
+    /**
+     * trigger exposed event
+     * @param {string} eventName
+     * @param {object} eventData
+     * @returns {*}
+     */
+    trigger(eventName, eventData) {
+        // console.log('evData:', evData);
+        let ev = new CustomEvent(eventName, { detail: eventData });
+        document.dispatchEvent(ev);
+    }
+    /**
+     * register before event
+     * @param {string} event
+     * @param {function} callback
+     */
+    before(event, callback) {
+        // if (typeof this.beforeMap !== 'object') this.beforeMap = {};
+        this.beforeMap[event] = callback;
+    }
+    /**
+     *register after event
+     * @param {string} event
+     * @param {function} callback
+     */
+    after(event, callback) {
+        // if (typeof this.afterMap !== 'object') this.afterMap = {};
+        this.afterMap[event] = callback;
+    }
+}
+
 // private
 // registry filled and emptied when attaching and unattaching components to DOM
 // renaissance.registry = {};
@@ -23,7 +97,6 @@ renaissance.drinks = {};
 renaissance.registerComponent = function(component, node) {
     // set node
     component.setNode(node);
-    console.log(component);
     // event handlers
     if (typeof component.afterMap['init'] === 'function') {
         component.afterMap['init'](component);
@@ -76,113 +149,6 @@ renaissance.registerPlugin = function(name, plugin) {
     return false;
 };
 
-// private
-/** wrapper for bare component definition
- * @param {object} Component
- * @returns {object} component
- */
-renaissance.defineComponent = function(Component) {
-    return new renaissance.Attachable(Component);
-};
-/**
- * component
- * @constructor
- * @returns {object} this
- */
-renaissance.Component = function() {
-    // this.beforeMap = {};
-    // this.afterMap = {};
-
-    return this;
-};
-renaissance.Component.prototype.beforeMap = {};
-renaissance.Component.prototype.afterMap = {};
-/**
- * set node
- * @param {object} node
- * @returns {*}
- */
-renaissance.Component.prototype.setNode = function(node) {
-    this.node = node;
-};
-/**
- * add an event handler on component
- * @param {string} nodeEvent
- * @param {function} callback
- * @returns {*}
- */
-renaissance.Component.prototype.on = function(nodeEvent, callback) {
-    this.node.addEventListener(nodeEvent, callback);
-};
-/**
- * expose an event to other components and make it triggerable
- * (for custom events)
- * @param {string} eventName
- * @param {function} callback
- * @returns {*}
- */
-renaissance.Component.prototype.expose = function(eventName, callback) {
-    document.addEventListener(evName, function(e) { cb(e, e.detail); });
-};
-// trigger exposed events
-/**
- * trigger exposed event
- * @param {string} eventName
- * @param {object} eventData
- * @returns {*}
- */
-renaissance.Component.prototype.trigger = function(eventName, eventData) {
-    // console.log('evData:', evData);
-    let ev = new CustomEvent(eventName, { detail: eventData });
-    document.dispatchEvent(ev);
-};
-/**
- * register before event
- * @param {string} event
- * @param {function} callback
- */
-renaissance.Component.prototype.before = function(event, callback) {
-    // if (typeof this.beforeMap !== 'object') this.beforeMap = {};
-    this.beforeMap[event] = callback;
-};
-/**
- *register after event
- * @param {string} event
- * @param {function} callback
- */
-renaissance.Component.prototype.after = function(event, callback) {
-    // if (typeof this.afterMap !== 'object') this.afterMap = {};
-    this.afterMap[event] = callback;
-};
-/**
- * Attachable class
- * @constructor
- * @param {object} Blueprint
- * @returns {object} this
- */
-renaissance.Attachable = function(Blueprint) {
-    this.Blueprint = Blueprint;
-    this.Blueprint.prototype = renaissance.Component.prototype;
-
-    return this;
-};
-/**
- * attach component to selected elements
- * @param {string} selector
- * @returns {array} components
- */
-renaissance.Attachable.prototype.attachTo = function(selector) {
-    let componentArr = [];
-    let nodes = renaissance.utils.getNodes(selector);
-    // register a component for every found node
-    for (let i = 0; i < nodes.length; i++) {
-        let component = renaissance.registerComponent(new this.Blueprint(), nodes[i]);
-        componentArr.push(component);
-    }
-
-    return componentArr;
-};
-
 // logger public
 
 // debug (depends on logger) public
@@ -220,16 +186,32 @@ renaissance.utils.getNodes = function(selector) {
         return document.getElementsByTagName(selector);
     }
 };
+/**
+ * attach component to selected elements
+ * @param {object} component
+ * @param {string} selector
+ * @returns {array} components
+ */
+renaissance.utils.attachTo = function(component, selector) {
+    let componentArr = [];
+    let nodes = renaissance.utils.getNodes(selector);
+    // register a component for every found node
+    for (let i = 0; i < nodes.length; i++) {
+        renaissance.registerComponent(component, nodes[i]); // this.Blueprint
+        componentArr.push(component);
+    }
+
+    return componentArr;
+};
 
 const r = renaissance;
-const defineComponent = renaissance.defineComponent;
 const registerAdapter = renaissance.registerAdapter;
 const utils = renaissance.utils;
 
 export {
     renaissance
     , r
-    , defineComponent
+    , Component
     , registerAdapter
     , utils
 }
