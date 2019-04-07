89d69a7c3c27c76f6dc3ecd22fceca5a720de608
frncsdrk <frncsdrk@protonmail.com>
2019-04-07 13:18:08 +0200

    Add editorconfig
    fix formatting

--- demo/src/components/BrowserComponent.js
+++ demo/src/components/BrowserComponent.js
@@ -1,46 +1,46 @@
 const { Component } = require('../../../dist/renaissance')
 
 class BrowserComponent extends Component {
-    /**
-     * component
-     * @constructor
-     * @returns {object} this
-     */
-    constructor() {
-        super();
-        this.nodes = [];
-        return this;
-    }
-    /**
-     * set node
-     * @param {object} node
-     * @returns {*}
-     */
-    addNode(node) {
-        this.nodes.push(node);
-    }
-    /**
-     * expose an event to other components and make it triggerable
-     * (for custom events)
-     * @param {string} eventName
-     * @param {function} callback
-     * @returns {*}
-     */
-    expose(eventName, callback) {
-        document.addEventListener(eventName, function(e) { callback(e, e.detail); });
-    }
-    // trigger exposed events
-    /**
-     * trigger exposed event
-     * @param {string} eventName
-     * @param {object} eventData
-     * @returns {*}
-     */
-    triggerExposed(eventName, eventData) {
-        // console.log('evData:', evData);
-        let ev = new CustomEvent(eventName, { detail: eventData });
-        document.dispatchEvent(ev);
-    }
+  /**
+   * component
+   * @constructor
+   * @returns {object} this
+   */
+  constructor() {
+    super();
+    this.nodes = [];
+    return this;
+  }
+  /**
+   * set node
+   * @param {object} node
+   * @returns {*}
+   */
+  addNode(node) {
+    this.nodes.push(node);
+  }
+  /**
+   * expose an event to other components and make it triggerable
+   * (for custom events)
+   * @param {string} eventName
+   * @param {function} callback
+   * @returns {*}
+   */
+  expose(eventName, callback) {
+    document.addEventListener(eventName, function (e) { callback(e, e.detail); });
+  }
+  // trigger exposed events
+  /**
+   * trigger exposed event
+   * @param {string} eventName
+   * @param {object} eventData
+   * @returns {*}
+   */
+  triggerExposed(eventName, eventData) {
+    // console.log('evData:', evData);
+    let ev = new CustomEvent(eventName, { detail: eventData });
+    document.dispatchEvent(ev);
+  }
 }
 
 module.exports = BrowserComponent
--- demo/src/components/HelloWorld.js
+++ demo/src/components/HelloWorld.js
@@ -3,25 +3,25 @@ const BrowserComponent = require('./BrowserComponent')
 const helloMixin = require('../mixins/hello-mixin')
 
 class HelloWorld extends BrowserComponent {
-    constructor() {
-        super();
-        console.log('nodes:', this.nodes);
-        this.meta = {
-            localStorage: r.adapters.localStorage
-        };
-        return this;
-    }
+  constructor() {
+    super();
+    console.log('nodes:', this.nodes);
+    this.meta = {
+      localStorage: r.adapters.localStorage
+    };
+    return this;
+  }
 
-    render() {
-        console.log('HelloWorld component render:', this.nodes)
-        for (let i = 0; i < this.nodes.length; i++) {
-            this.nodes[i].innerHTML = 'hello world';
-        }
+  render() {
+    console.log('HelloWorld component render:', this.nodes)
+    for (let i = 0; i < this.nodes.length; i++) {
+      this.nodes[i].innerHTML = 'hello world';
     }
+  }
 
-    init() {
-        this.render();
-    }
+  init() {
+    this.render();
+  }
 }
 
 const registered = r.registerMixin('sayHello', HelloWorld, helloMixin);
