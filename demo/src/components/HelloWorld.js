dc0d0ad63f6a085520adb65ce2722f9ec4562c7c
frncsdrk <frncsdrk@protonmail.com>
2019-04-27 18:06:37 +0200

    Fix formatting

--- demo/src/components/HelloWorld.js
+++ demo/src/components/HelloWorld.js
@@ -3,24 +3,24 @@ import BrowserComponent from './BrowserComponent'
 import helloMixin from '../mixins/hello-mixin'
 
 class HelloWorld extends BrowserComponent {
-    constructor() {
-        super();
-        console.log('nodes:', this.nodes);
-        this.meta = {
-            localStorage: r.adapters.localStorage
-        };
-        return this;
-    }
+  constructor() {
+    super();
+    console.log('nodes:', this.nodes);
+    this.meta = {
+      localStorage: r.adapters.localStorage
+    };
+    return this;
+  }
 
-    render() {
-        for (let i = 0; i < this.nodes.length; i++) {
-            this.nodes[i].innerHTML = 'hello world';
-        }
+  render() {
+    for (let i = 0; i < this.nodes.length; i++) {
+      this.nodes[i].innerHTML = 'hello world';
     }
+  }
 
-    init(component) {
-        component.render();
-    }
+  init(component) {
+    component.render();
+  }
 }
 
 const registered = r.registerMixin('sayHello', HelloWorld, helloMixin);
